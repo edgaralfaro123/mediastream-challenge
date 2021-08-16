d51653132ba99d25cdce2f765cddbd5ad622455f
Edgar alfaro <alfa-_01@outlook.es>
2021-08-15 23:11:09 -0500

    feat: agrego ejercicio 02-javascript

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -7,16 +7,33 @@ console.log(`
 Take a look at the 'requester' function.
 
 - What it does?
+  Realiza una peticion a una api
 - How it's used? Add different use-case examples that covers every functionality.
+
 - How it is called this design pattern or technique?
+  Patron es factory o fabrica porque da una clase o método de acuerdo a los argumentos.
 
 HINT: Use https://api.github.com/users/mediastream
 `);
 
 // Add fetch polyfill for Node.js
 require('isomorphic-fetch'); // See: https://github.com/matthew-andrews/isomorphic-fetch
+const path = ['users','mediastream'];
+const base ='https://api.github.com';
 
 function requester(method, base, headers = { Accept: '*/*' }) {
-  return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
-    .then(r => r.json());
+  return  (path = []) => {
+    fetch((base ? [base, ...path] : path).join('/'), { method, headers })
+    .then(r => {
+      r.json()})
+  }
 }
+
+const method='GET';
+
+const result = requester(method,base)
+console.log('result',result)
+
+
+
+
